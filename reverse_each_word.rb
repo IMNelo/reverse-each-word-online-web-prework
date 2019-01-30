d9db7ea606469c40f40af1dad1622a711bdc5891
Agnelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-01-30 06:51:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,7 @@
+sentence1 = ["Hello there, and how are you?"]
+
 def reverse_each_word(sentence)
-  sentence.each do |word|
-    word.reverse
+	sentence.each do |word|
+		sentence.reverse
+	end
 end
