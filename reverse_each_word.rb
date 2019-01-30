36f6b9349ce0a1fd75e739fa06771b00879ac588
Agnelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-01-30 07:38:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,11 @@
-def reverse_each_word(sentence)
-	sentence.each do |x|
+def reverse_each_word(sentence1)
+	sentence1.each do |x|
+		x.reverse!
+	end
+end
+
+def reverse_each_word(sentence2)
+	sentence2.each do |x|
 		x.reverse!
 	end
 end
